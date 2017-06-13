c178d3785c985a31f409fea65850e677d072085e
Daonkey-Tao <taofei0610@gmail.com>
2017-06-13 16:59:34 +0800

    updata

--- MBProgressHUD-DBExtension/MBProgressHUD+DBExtension.h
+++ MBProgressHUD-DBExtension/MBProgressHUD+DBExtension.h
@@ -5,18 +5,14 @@
 @interface MBProgressHUD (DBExtension)
 
 
+#pragma mark - 提示内容单行的HUD
 
 + (void)db_showSuccess:(NSString *)success toView:(UIView *)view;
 + (void)db_showError:(NSString *)error toView:(UIView *)view;
-
 + (void)db_showMessage:(NSString *)message toView:(UIView *)view;
-
-
 + (void)db_showSuccess:(NSString *)success;
 + (void)db_showError:(NSString *)error;
-
 + (void)db_showMessage:(NSString *)message;
-
 + (void)db_hideHUDForView:(UIView *)view;
 + (void)db_hideHUD;
 +(void)db_hideHUDAnimated:(BOOL)animated;
@@ -26,9 +22,7 @@
 #pragma mark - 提示内容可以换行的HUD
 
 + (void)db_showMultiLineSuccess:(NSString *)success toView:(UIView *)view;
-
 + (void)db_showMultiLineError:(NSString *)error toView:(UIView *)view;
-
 + (void)db_showMultiLineMessage:(NSString *)message toView:(UIView *)view;
 + (void)db_showMultiLineSuccess:(NSString *)success;
 + (void)db_showMultiLineError:(NSString *)error;
